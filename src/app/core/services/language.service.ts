e1a887f9091976a1cdf6f10665ff4fc0c33faec9
Kencho5 <giokenchadze@gmail.com>
2024-12-06 20:05:22 +0400

    Added: translation

--- src/app/core/services/language.service.ts
+++ src/app/core/services/language.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { signal } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LanguageService {
+  constructor(private translate: TranslateService) {}
   private lang = signal(localStorage.getItem('lang') || 'ge');
 
   get currentLang() {
@@ -13,6 +15,7 @@ export class LanguageService {
 
   setLanguage(lang: string) {
     this.lang.set(lang);
+    this.translate.use(lang);
     localStorage.setItem('lang', lang);
   }
 }
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../auth/services/auth.service';
 import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, CommonModule, LanguageSelectorComponent],
+  imports: [RouterLink, CommonModule, LanguageSelectorComponent, TranslatePipe],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
