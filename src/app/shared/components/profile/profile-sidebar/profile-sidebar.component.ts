0e2b9d8fa60f5fe05e4e92c95bf1ecc4a00d27e7
Kencho5 <giokenchadze@gmail.com>
2025-02-07 00:10:13 +0400

    Added: active link in sidebar

--- src/app/shared/components/profile/profile-sidebar/profile-sidebar.component.ts
+++ src/app/shared/components/profile/profile-sidebar/profile-sidebar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 import { SharedModule } from '@shared/shared.module';
 
 @Component({
   selector: 'app-profile-sidebar',
-  imports: [SharedModule],
+  imports: [SharedModule, RouterModule],
   templateUrl: './profile-sidebar.component.html',
 })
-export class ProfileSidebarComponent {}
+export class ProfileSidebarComponent {
+  constructor(public authService: AuthService) {}
+}
