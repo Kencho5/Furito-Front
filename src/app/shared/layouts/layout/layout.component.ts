063ea28650f4968da5c22729ecbcb7d4e7e4380d
Kencho5 <giokenchadze@gmail.com>
2025-02-08 00:13:33 +0400

    Added: footer

--- src/app/shared/layouts/layout/layout.component.ts
+++ src/app/shared/layouts/layout/layout.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '@shared/components/footer/footer.component';
 import { NavbarComponent } from '@shared/components/navbar/navbar.component';
 import { LoadingDotsComponent } from '@ui/loading-dots/loading-dots.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, NavbarComponent, LoadingDotsComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    LoadingDotsComponent,
+    FooterComponent,
+  ],
   templateUrl: './layout.component.html',
 })
 export class LayoutComponent {}
