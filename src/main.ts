1a91cc5ec04032a3919fd5bdc10563a4ce2222ae
Kencho5 <giokenchadze@gmail.com>
2025-02-08 13:39:02 +0400

    Test: added posthog

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import posthog from 'posthog-js';
+
+posthog.init('phc_n5MDfwnLzZtpIJvbXtkgTuOWWAU6qPeqB5jIfikfP79', {
+  api_host: 'https://us.i.posthog.com',
+});
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
