bdb9c55b4ea3eb2175704df8399c7f61ad3b22b9
Kencho5 <giokenchadze@gmail.com>
2025-02-09 10:31:33 +0400

    Added: search on submit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslatePipe } from '@ngx-translate/core';
 import { ImageComponent } from '@shared/components/image/image.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, TranslatePipe, ImageComponent],
+  imports: [CommonModule, TranslatePipe, ImageComponent, ReactiveFormsModule],
   templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private router: Router) {}
+
+  searchForm = new FormGroup({
+    text: new FormControl(''),
+  });
+
+  search(): void {
+    const query = this.searchForm.value.text?.trim();
+
+    if (query) {
+      this.router.navigate(['/search'], { queryParams: { text: query } });
+    }
+  }
+}
