bb08f94c1c9cad424ddd99dd007e0175ff10ffc6
Kencho5 <giokenchadze@gmail.com>
2025-02-09 10:54:58 +0400

    Added: home services

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { TranslatePipe } from '@ngx-translate/core';
 import { ImageComponent } from '@shared/components/image/image.component';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { SERVICES } from '@utils/homeServices';
+import { SharedModule } from '@shared/shared.module';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, TranslatePipe, ImageComponent, ReactiveFormsModule],
+  imports: [SharedModule, TranslatePipe, ImageComponent, ReactiveFormsModule],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
   constructor(private router: Router) {}
+  SERVICES = SERVICES;
 
   searchForm = new FormGroup({
     text: new FormControl(''),
---
+++ src/app/utils/homeServices.ts
@@ -0,0 +1,44 @@
+export const SERVICES = [
+  {
+    url: '',
+    title: 'შეიფარე',
+    subtext: 'აქ წამოვა მცირედი აღწერა სერვისის შესახებ',
+    icon: '/icons/home/house.svg',
+    color: 'bg-pink-100',
+  },
+  {
+    url: '',
+    title: 'შეიძინე',
+    subtext: 'აქ წამოვა მცირედი აღწერა სერვისის შესახებ',
+    icon: '/icons/home/price.svg',
+    color: 'bg-lime-100',
+  },
+  {
+    url: '',
+    title: 'შეაჯვარე',
+    subtext: 'აქ წამოვა მცირედი აღწერა სერვისის შესახებ',
+    icon: '/icons/home/heart.svg',
+    color: 'bg-orange-100',
+  },
+  {
+    url: '',
+    title: 'დაიკარგა',
+    subtext: 'აქ წამოვა მცირედი აღწერა სერვისის შესახებ',
+    icon: '/icons/home/megaphone.svg',
+    color: 'bg-sky-100',
+  },
+  {
+    url: '',
+    title: 'ზოომაღაზია',
+    subtext: 'აქ წამოვა მცირედი აღწერა სერვისის შესახებ',
+    icon: '/icons/home/bag.svg',
+    color: 'bg-yellow-100',
+  },
+  {
+    url: '',
+    title: 'ზრუნვა',
+    subtext: 'აქ წამოვა მცირედი აღწერა სერვისის შესახებ',
+    icon: '/icons/home/hand.svg',
+    color: 'bg-indigo-100',
+  },
+];
