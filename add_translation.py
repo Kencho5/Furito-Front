10aacdc26ae3447c4d8c50def127966cb386495c
Kencho5 <giokenchadze@gmail.com>
2025-02-09 11:08:49 +0400

    Updated: add translation to both files

--- add_translation.py
+++ add_translation.py
@@ -1,36 +1,51 @@
 import json
 
 
-def update_json(file_path, key_path, value):
-    with open(file_path, "r", encoding="utf-8") as file:
-        data = json.load(file)
+def update_json(en_path, ge_path, key_path, en_value, ge_value):
+    with open(en_path, "r", encoding="utf-8") as file:
+        en_data = json.load(file)
+
+    with open(ge_path, "r", encoding="utf-8") as file:
+        ge_data = json.load(file)
 
     path_parts = key_path.split(".")
-    current = data
-    for _, part in enumerate(path_parts[:-1]):
-        if part not in current:
-            current[part] = {}
-        current = current[part]
 
-    current[path_parts[-1]] = value
+    current_en = en_data
+    for part in path_parts[:-1]:
+        if part not in current_en:
+            current_en[part] = {}
+        current_en = current_en[part]
+    current_en[path_parts[-1]] = en_value
 
-    with open(file_path, "w", encoding="utf-8") as file:
-        json.dump(data, file, ensure_ascii=False, separators=(",", ":"))
+    current_ge = ge_data
+    for part in path_parts[:-1]:
+        if part not in current_ge:
+            current_ge[part] = {}
+        current_ge = current_ge[part]
+    current_ge[path_parts[-1]] = ge_value
 
-    print(f"Successfully added {path_parts[-1]}: {value}")
+    with open(en_path, "w", encoding="utf-8") as file:
+        json.dump(en_data, file, ensure_ascii=False, separators=(",", ":"))
+
+    with open(ge_path, "w", encoding="utf-8") as file:
+        json.dump(ge_data, file, ensure_ascii=False, separators=(",", ":"))
+
+    print(f"Successfully added {path_parts[-1]}")
+    print(f"EN: {en_value}")
+    print(f"GE: {ge_value}")
 
 
 def main():
-    file_path = f"public/i18n/{input('Enter language: ')}.json"
+    en_path = "public/i18n/en.json"
+    ge_path = "public/i18n/ge.json"
 
     try:
         while True:
             key_path = input("Enter the key path: ")
-            value = input("Enter the value: ")
-
-            update_json(file_path, key_path, value)
+            en_value = input("Enter English value: ")
+            ge_value = input("Enter Georgian value: ")
+            update_json(en_path, ge_path, key_path, en_value, ge_value)
             print("---")
-
     except KeyboardInterrupt:
         print("\nExiting...")
 
