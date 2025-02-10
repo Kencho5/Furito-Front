72139760acf05593f31d6bf538a1c693bbfd068b
Kencho5 <giokenchadze@gmail.com>
2025-02-10 17:03:47 +0400

    Updated: 404 page design

--- src/app/shared/components/not-found/not-found.component.ts
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
+import { ImageComponent } from '../image/image.component';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
-  imports: [SharedModule],
+  imports: [SharedModule, ImageComponent],
   templateUrl: './not-found.component.html',
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  constructor(private location: Location) {}
+
+  back(): void {
+    this.location.back();
+  }
+}
