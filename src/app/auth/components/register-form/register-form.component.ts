68e10d209d7d9aa0926b868f1bbd89bde86104ce
Kencho5 <giokenchadze@gmail.com>
2025-02-14 10:19:33 +0400

    Added: debug

--- src/app/auth/components/register-form/register-form.component.ts
+++ src/app/auth/components/register-form/register-form.component.ts
@@ -130,6 +130,7 @@ export class RegisterFormComponent {
           this.authService.login(response.token, false);
         },
         error: (response: HttpErrorResponse) => {
+          console.log(response);
           if (response.status == 429) {
             this.formError.set('AUTH.ERROR.limit');
             return;
